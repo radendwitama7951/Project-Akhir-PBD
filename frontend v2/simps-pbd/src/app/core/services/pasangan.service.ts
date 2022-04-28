0db40fb1fa4b15f217b9af49b30759cea67052b2
radendwitama7951 <96223796+dwitamabaliano045@users.noreply.github.com>
2022-04-28 23:42:33 +0800

    ADD custom post method

--- frontend v2/simps-pbd/src/app/core/services/pasangan.service.ts
+++ frontend v2/simps-pbd/src/app/core/services/pasangan.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {
   EntityCollectionServiceBase,
@@ -5,6 +6,7 @@ import {
 } from '@ngrx/data';
 import { Observable } from 'rxjs';
 import { first, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 import { PasanganInterface } from '../interfaces/pasangan.interface';
 
@@ -12,10 +14,78 @@ import { PasanganInterface } from '../interfaces/pasangan.interface';
   providedIn: 'root',
 })
 export class PasanganService extends EntityCollectionServiceBase<PasanganInterface> {
-  constructor(serviceElementFactory: EntityCollectionServiceElementsFactory) {
+  private pasanganApiUrl: string = environment.apiUrl + 'pasangan/';
+  private statusPasangan: string[] = ['mantan', 'pacar', 'selingkuhan'];
+
+  constructor(
+    serviceElementFactory: EntityCollectionServiceElementsFactory,
+    private httpClient: HttpClient
+  ) {
     super('Pasangan', serviceElementFactory);
   }
 
+  /* @param data PasanganInterface
+   *
+   * @types Method
+   * Pengganti bug ngrx/data add
+   *
+   * */
+  public post(data: PasanganInterface): void {
+    let dataSerializer: PasanganInterface = {
+      pasangan_id: 0,
+      first_name: '',
+      last_name: '',
+      special_name: '',
+      avatar: '',
+      kencan_terakhir: 'belum pernah kencan',
+      status_pasangan_id: 0,
+      status_pasangan: this.getStatusPasangan(data.status_pasangan_id),
+    };
+
+    this.keys$.subscribe((ids: number[]) => {
+      dataSerializer.pasangan_id = Math.max.apply(null, ids) + 1;
+    });
+
+    this.httpClient
+      .post(this.pasanganApiUrl, { ...dataSerializer, ...data })
+      .subscribe(console.log);
+    this.addOneToCache({ ...dataSerializer, ...data });
+    this.entities$.subscribe(console.log);
+    this.keys$.subscribe(console.log);
+  }
+
+  /* @param data PasanganInterface
+   *
+   * @types Method
+   * Pengganti bug ngrx/data add
+   *
+   * */
+  public put(data: PasanganInterface): void {
+    let dataSerializer: PasanganInterface = {
+      pasangan_id: 0,
+      first_name: '',
+      last_name: '',
+      special_name: '',
+      avatar: '',
+      kencan_terakhir: 'belum pernah kencan',
+      status_pasangan_id: 0,
+      status_pasangan: this.getStatusPasangan(data.status_pasangan_id),
+    };
+
+    this.keys$.subscribe((ids: number[]) => {
+      dataSerializer.pasangan_id = Math.max.apply(null, ids) + 1;
+    });
+
+    this.update({ ...dataSerializer, ...data });
+  }
+
+  /* @status Deprecated
+   * @types Utils
+   * @param pasangan_id number
+   * untuk mendapatkan data special_name berupa string
+   * pada tabel pasangan
+   *
+   * */
   public getSpecialName(pasangan_id: number): string {
     let specialName!: string;
     this.getByKey(pasangan_id).subscribe(
@@ -24,10 +94,26 @@ export class PasanganService extends EntityCollectionServiceBase<PasanganInterfa
     return specialName;
   }
 
+  /* @type Utils
+   * @param pasangan_id Number
+   *
+   * ambil data pasangan dari cache
+   *
+   * */
   public selectEntityById(pasangan_id: number): Observable<PasanganInterface> {
     return this.entityMap$.pipe(
       map((entities) => entities[pasangan_id]),
       first()
     );
+  }
+
+  /* @types Utils
+   * @param status_pasangan_id Number
+   *
+   * return status pasangan dalam rupa string
+   * digunkan ketika post dan update
+   * */
+  public getStatusPasangan(status_pasangan_id: number): string {
+    return this.statusPasangan[status_pasangan_id];
   }
 }
